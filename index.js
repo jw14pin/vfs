14412113c3db986b156ea191f2ae566931673b98
Jonathan Wong <jwong14pin@gmail.com>
2014-10-04 12:03:54 -0500

    bla

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
+app.set('port', (process.env.PORT || 3000));
+
 app.get('/', function(req, res){
   res.sendFile(path.join(__dirname, '/', 'index.html'));
 });
