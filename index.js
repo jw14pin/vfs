e416cee6d5d5afd9668504b8e4ab449dcb74c8ac
Jonathan Wong <jwong14pin@gmail.com>
2014-10-04 12:16:43 -0500

    bla

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.get('/', function(req, res){
   res.sendFile(path.join(__dirname, '/', 'index.html'));
 });
 
+app.get('/flare.json', function(req, res){
+  res.sendFile(path.join(__dirname, '/', 'flare.json'));
+});
+
 http.listen(app.get('port'), function(){
   console.log('listening on *:'+app.get('port'));
 });
