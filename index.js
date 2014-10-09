d17762616fba9c3c01635672bd7df1ee3e0591b4
Jonathan Wong <jwong14pin@gmail.com>
2014-10-08 23:28:49 -0500

    message

--- index.js
+++ index.js
@@ -2,10 +2,14 @@
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 var path = require('path');
+var fs = require('fs');
+
+var ioConnections = 0;
 
 // app global variables
-var routes = require('./routes/index');
+//var routes = require('./routes/index');
 
 // listen on server node env port
 app.set('port', (process.env.PORT || 3000));
@@ -18,7 +22,15 @@ app.set('view engine', 'jade');
 app.use('/public',express.static(path.join(__dirname, 'public')));
 
 // routing for the app
-app.use('/', routes);
+//app.use('/', routes);
+
+app.get('/', function(req,res,next) {
+	res.render('index',
+		{
+			title:"Hello",
+			server:"test"
+		});
+});
 
 // error handling
 app.use(function(err,req,res,next) {
@@ -29,8 +41,54 @@ app.use(function(err,req,res,next) {
 	});
 });
 
-// start up the server
-var server = app.listen(app.get('port'), function() {
-	console.log('server is listening on port: ' + server.address().port);
+io.on('connection', function(socket) {
+	var jsonFS = {"name":"./vfs", "children":[]};
+	ioConnections++;
+	console.log(ioConnections +":"+socket.id);
+	getFS('./vfs', jsonFS, jsonFS, function(data) {
+		console.log(data);
+		io.emit('item', data);
+	});
+	
 });
 
+function getFS(path, jsonFS, current, fn) {
+	fs.readdir(path,function(err, items){
+		if(err == null && items.length > 0) {
+		
+			items.forEach(function(item){
+				setTimeout(function() {
+				
+					fs.lstat(path + '/' + item, function(err, itemStat) {
+						if (itemStat.isDirectory()) {
+							current.children.push({"name":item,"children":[]});
+							var dir = current.children[current.children.length-1];
+							getFS(path + '/' + item, jsonFS, dir, fn);
+						} else {
+							current.children.push({"name":item, "size":itemStat.size});
+							var dir = current.children[current.children.length-1];
+							getFS(path + '/' + item, jsonFS, dir, fn);
+							console.log('sent file');
+						}
+					});
+				
+				},500);
+			});
+		} else {
+			fn(jsonFS);
+		}
+	});	
+}
+
+function loopFunction(counter, itemsLength, fn) {
+	if(counter < itemsLength) {
+		setTimeout(loopFunction(counter, itemsLength, fn), 500);
+	} else {
+		fn;
+	}
+}
+
+// start up the server
+var server = http.listen(app.get('port'), function() {
+	console.log('server is listening on port: ' + server.address().port);
+});
